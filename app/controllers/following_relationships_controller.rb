ca3d6e85f554448d9d3da621d765b59c253af974
Catalin Boruga <catalinboruga@yahoo.com>
2016-03-16 20:37:47 +0100

    Add new resources

---
+++ app/controllers/following_relationships_controller.rb
@@ -0,0 +1,18 @@
+class FollowingRelationshipsController < ApplicationController
+
+  def create
+    current_user.follow(user)
+    redirect_to user, notice: 'Now following user'
+  end
+
+  def destroy
+    current_user.unfollow(user)
+    redirect_to user, notice: 'Now you unfollow the user'
+  end
+
+  private
+
+  def user
+    @_user ||= User.find(params[:user_id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  validates :email, presence: true, uniqueness: true
-  validates :password_digest, presence: true
-
+  include Following
   has_many :shouts
 end
